02de14d3e7b8c47b7d3930e6286c18b14d88fd45
Tan Theng Lee <thengleetan@gmail.com>
2015-08-20 13:33:45 +0800

    Sorcery authentication complete

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,8 @@ class CommentsController < ApplicationController
 
   include CommentsHelper
 
+  before_filter :require_login, except: [:create]
+
   def create
     @comment = Comment.new(comment_params)
     @comment.article_id = params[:article_id]
