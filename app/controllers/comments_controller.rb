0aaa4917307fa5b459f09e34f3bc0288875479d0
Tan Theng Lee <thengleetan@gmail.com>
2015-11-07 18:21:55 +0800

    Refactor comments create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,12 +3,24 @@ class CommentsController < ApplicationController
   include CommentsHelper
 
   before_filter :require_login, except: [:create]
+  before_filter :get_article, only: [:create]
 
   def create
-    @comment = Comment.new(comment_params)
-    @comment.article_id = params[:article_id]
-    @comment.save
-    redirect_to article_path(@comment.article)
+    if @comment = @article.comments.create(comment_params)
+      redirect_to article_path(@comment.article)
+    else
+      render @comment
+    end
+
+    # @comment = Comment.new(comment_params)
+    # puts "\n========\n#{@comment.inspect}\n========\n"
+
+    # @comment.article_id = params[:article_id]
+    # @comment.save
+    # redirect_to article_path(@comment.article)
   end
 
+  def get_article
+    @article = Article.find_by(id: params[:article_id])
+  end
 end
